cf342e8d152b39d4d235f1eb29f73065c6dd6fa5
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-05-30 12:59:31 +0300

    Update asd_task_29052024.py
    get rid of helper, simplified function

--- asd_task_29052024.py
+++ asd_task_29052024.py
@@ -2,20 +2,15 @@ import os
 
 def find_all_catalog_files_recursively(dir_path = '.'):
     file_paths = []
-    
-    def helper(dir_path):
-        names = os.listdir(dir_path)
-        for name in names:
-            if name.startswith('.'):
-                continue
-            path = os.path.join(dir_path, name)
-            if os.path.isdir(path):
-                helper(path)
-            else:
-                file_paths.append(path)
-        return file_paths
-    
-    helper(dir_path)
+    names = os.listdir(dir_path)
+    for name in names:
+        if name.startswith('.'):
+            continue
+        path = os.path.join(dir_path, name)
+        if os.path.isdir(path):
+            file_paths.extend(find_all_catalog_files_recursively(path))
+        else:
+            file_paths.append(path)
     return file_paths
 
 # to check
