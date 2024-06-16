ba85144138d840c8f7c6d60352c7506ddb3f1144
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-06-16 15:05:01 +0300

    Create aBST_13.py

---
+++ aBST_13.py
@@ -0,0 +1,31 @@
+class aBST:
+
+    def __init__(self, depth):
+        tree_size = 2**(depth+1) - 1
+        self.Tree = [None] * tree_size
+	
+    def FindKeyIndex(self, key):
+        def helper(index, key):
+            if index >= len(self.Tree):
+                return None
+            if self.Tree[index] is None:
+                return -index
+            if self.Tree[index] == key:
+                return index 
+                
+            if key < self.Tree[index]:
+                index = index*2 + 1 
+            else:
+                index = index*2 + 2
+            return helper(index, key)
+        return helper(0, key) 
+	
+    def AddKey(self, key):
+        index = self.FindKeyIndex(key)
+        if index is None:
+            return -1
+        if (index == 0 and self.Tree[index] is None) or index < 0:
+            index *= -1
+            self.Tree[index] = key
+        return index
+        
