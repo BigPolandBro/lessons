cc17085604887354fbbceac505bd928fef8cc261
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-08-08 10:45:29 +0300

    Create seq_shell_sort_24.py

---
+++ seq_shell_sort_24.py
@@ -0,0 +1,27 @@
+def KnuthSequence(array_size):
+    i = 1
+    seq = []
+    while i < array_size:
+        seq.append(i)
+        i = i*3 + 1
+    seq.reverse()
+    return seq
+    
+def InsertionSortStep(array, step, i):
+    n = len(array)
+    for j in range(i+step, n, step):
+        first_bigger = j + 1
+        for k in range(i, j, step):
+            if array[j] < array[k]:
+                first_bigger = k 
+                break
+        for k in range(j, first_bigger, -step):
+            array[k], array[k-step] = array[k-step], array[k]
+    
+def ShellSort(array):
+    seq = KnuthSequence(len(array))
+    for step in seq:
+        for i in range(0, step):
+            InsertionSortStep(array, step, i)
+            
+            
