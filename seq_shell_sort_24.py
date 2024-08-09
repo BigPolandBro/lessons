a9118c418512c9efbe1892911099443a88a3c624
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-08-09 15:44:23 +0300

    Update seq_shell_sort_24.py

--- seq_shell_sort_24.py
+++ seq_shell_sort_24.py
@@ -1,4 +1,4 @@
-def KnuthSequence(array_size):
+def KnuthSequence(array_size: int) -> 'list[int]':
     i = 1
     seq = []
     while i < array_size:
@@ -6,22 +6,23 @@ def KnuthSequence(array_size):
         i = i*3 + 1
     seq.reverse()
     return seq
-    
-def InsertionSortStep(array, step, i):
+
+
+def InsertionSortStep(array: 'list[int]', step: int, i: int) -> None:
     n = len(array)
-    for j in range(i+step, n, step):
+    for j in range(i + step, n, step):
         first_bigger = j + 1
         for k in range(i, j, step):
             if array[j] < array[k]:
-                first_bigger = k 
+                first_bigger = k
                 break
         for k in range(j, first_bigger, -step):
-            array[k], array[k-step] = array[k-step], array[k]
-    
-def ShellSort(array):
+            array[k], array[k - step] = array[k - step], array[k]
+
+
+def ShellSort(array: 'list[int]') -> None:
     seq = KnuthSequence(len(array))
     for step in seq:
         for i in range(0, step):
             InsertionSortStep(array, step, i)
-            
-            
+
