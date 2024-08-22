f30e0cb67932c832d2438d84dff190a6207fa8db
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-08-22 16:02:17 +0300

    Create quick_sort_tro_27.py

---
+++ quick_sort_tro_27.py
@@ -0,0 +1,32 @@
+def ArrayChunk(M, left, right):
+    if left > right:
+        return -1
+    N = M[right]
+    i = left - 1
+    for j in range(left, right):
+        if M[j] <= N:
+            i += 1
+            M[i], M[j] = M[j], M[i]
+    M[i+1], M[right] = M[right], M[i+1]
+    return i+1
+
+def QuickSortTailOptimization(M, left, right):
+    def start():
+        nonlocal left, right
+        if left >= right:
+            return
+        
+        iN = ArrayChunk(M, left, right)
+        if iN - left < right - iN:
+            QuickSortTailOptimization(M, left, iN - 1)
+            left = iN + 1 
+        else:
+            QuickSortTailOptimization(M, iN + 1, right)
+            right = iN - 1
+            
+        start()
+        
+    start()
+
+
+
