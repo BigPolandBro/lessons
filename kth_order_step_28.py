1298dad7a0513c7fe13833777814d437bf86e0dc
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-08-25 19:11:59 +0300

    Create kth_order_step_28.py

---
+++ kth_order_step_28.py
@@ -0,0 +1,26 @@
+def ArrayChunk(array, left, right):
+    if left > right:
+        return -1
+
+    pivot_ind = (left+right)//2
+    pivot = array[pivot_ind]
+    array[pivot_ind], array[right] = array[right], array[pivot_ind]
+    i = left - 1
+    for j in range(left, right):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i+1], array[right] = array[right], array[i+1]
+    return i + 1
+
+def KthOrderStatisticsStep(Array, L, R, k):
+    iN = ArrayChunk(Array, L, R)
+    if iN == k:
+        return [iN, iN]
+    if iN < k:
+        return [iN+1, R]
+    if iN > k:
+        return [L, iN-1]
+
+
+
