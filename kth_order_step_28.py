efa49befc6d0e2bdc57558faf6e9ade2ab4c1cc4
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-08-27 17:32:32 +0300

    Update kth_order_step_28.py

--- kth_order_step_28.py
+++ kth_order_step_28.py
@@ -1,17 +1,30 @@
-def ArrayChunk(array: 'list[int]', left: int, right: int) -> int:
-    if left > right:
-        return -1
+def swap_elements(M: 'list[int]', i1: int, i2: int, iN: int) -> int:
+    M[i1], M[i2] = M[i2], M[i1]
+    new_iN = iN
+    if i1 == iN:
+        new_iN = i2
+    if i2 == iN:
+        new_iN = i1
+    return new_iN
 
-    pivot_ind = (left + right) // 2
-    pivot = array[pivot_ind]
-    array[pivot_ind], array[right] = array[right], array[pivot_ind]
-    i = left - 1
-    for j in range(left, right):
-        if array[j] <= pivot:
-            i += 1
-            array[i], array[j] = array[j], array[i]
-    array[i + 1], array[right] = array[right], array[i + 1]
-    return i + 1
+
+def ArrayChunk(M: 'list[int]', left: int, right: int) -> int:
+    iN = (left + right) // 2
+    i1 = left
+    i2 = right
+    while i1 <= i2:
+        while M[i1] < M[iN]:
+            i1 += 1
+        while M[i2] > M[iN]:
+            i2 -= 1
+        if i1 == i2 - 1 and M[i1] > M[i2]:
+            M[i1], M[i2] = M[i2], M[i1]
+            iN = ArrayChunk(M, left, right)
+            return iN
+        if i1 == i2 or (i1 == i2 - 1 and M[i1] < M[i2]):
+            return iN
+        iN = swap_elements(M, i1, i2, iN)
+    return iN
 
 
 def KthOrderStatisticsStep(Array: 'list[int]', L: int, R: int, k: int) -> 'list[int]':
