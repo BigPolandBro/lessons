0c4580eeb2ab791fef51358225d243f63856a157
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-08-28 09:55:40 +0300

    Update tests_28_k_stat.py

--- tests_28_k_stat.py
+++ tests_28_k_stat.py
@@ -3,6 +3,39 @@ import itertools
 
 from main import swap_elements, ArrayChunk, KthOrderStatisticsStep
 
+def ArrayChunkLomuto(array, left, right):
+    if left > right:
+        return -1
+
+    pivot_ind = (left+right)//2
+    pivot = array[pivot_ind]
+    array[pivot_ind], array[right] = array[right], array[pivot_ind]
+    i = left - 1
+    for j in range(left, right):
+        if array[j] <= pivot:
+            i += 1
+            array[i], array[j] = array[j], array[i]
+    array[i+1], array[right] = array[right], array[i+1]
+    return i + 1
+
+def ArrayChunkL4(M):
+    iN = (len(M))//2
+    i1 = 0
+    i2 = len(M) - 1
+    while i1 <= i2:
+        while M[i1] < M[iN]:
+            i1 += 1
+        while M[i2] > M[iN]:
+            i2 -= 1
+        if i1 == i2 - 1 and M[i1] > M[i2]:
+            M[i1], M[i2] = M[i2], M[i1]
+            iN = ArrayChunk_(M)
+            return iN
+        if i1 == i2 or (i1 == i2 - 1 and M[i1] < M[i2]):
+            return iN
+        iN = swap_elements(M, i1, i2, iN)
+    return iN
+
 def ArrayChunk_(M):
     iN = (len(M)-1)//2
     i1 = 0
@@ -71,12 +104,27 @@ class TestKthOrderStatisticsStep(unittest.TestCase):
                     self.assertEqual(iN, iN2)
 
     def test_custom(self):
-        a = [4, 5, 6, 3, 1, 2]
+        a = [5, 6, 4, 1, 2, 3]
         newLR = KthOrderStatisticsStep(a, 0, 5, 0)
-        self.assertEqual(a, [4, 5, 2, 3, 1, 6])
-        self.assertTrue(self.check_array_chunk(a, 5, 0, 5))
+        self.assertEqual(a, [1, 2, 3, 4, 6, 5])
+        self.assertTrue(self.check_array_chunk(a, 0, 0, 5))
         self.assertEqual(newLR[0], 0)
-        self.assertEqual(newLR[1], 4)
+        self.assertEqual(newLR[1], 0)
+
+        a = [5, 6, 4, 1, 2, 3]
+        iN = ArrayChunkL4(a)
+        self.assertEqual(a, [1, 6, 4, 5, 2, 3])
+        self.assertEqual(iN, 0)
+
+        a = [5, 6, 4, 1, 2, 3]
+        iN = ArrayChunkLomuto(a, 0, 5)
+        self.assertEqual(a, [3, 1, 2, 4, 5, 6])
+        self.assertEqual(iN, 3)
+
+        a = [5, 6, 4, 1, 2, 3]
+        iN = ArrayChunk_(a)
+        self.assertEqual(a, [1, 2, 3, 4, 6, 5])
+        self.assertEqual(iN, 0)
 
         a = [5, 6, 7, 4, 1, 2, 3]
         newLR = KthOrderStatisticsStep(a, 0, 6, 0)
