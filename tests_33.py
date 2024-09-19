78b60f2dbb68733e4b313c2eacfeb47737ca8a50
BigPolandBro <43274114+BigPolandBro@users.noreply.github.com>
2024-09-19 14:44:52 +0300

    Update tests_33.py

--- tests_33.py
+++ tests_33.py
@@ -2,7 +2,7 @@ import unittest
 import itertools
 import random
 
-from main import BinarySearch
+from main import GallopingSearch
 
 class TestBinarySearch(unittest.TestCase):
     def generate_permutations(self, n):
@@ -23,60 +23,52 @@ class TestBinarySearch(unittest.TestCase):
         return array
 
     def test_small(self):
-        bs = BinarySearch([])
-
         a = []
-        self.assertFalse(bs.GallopingSearch(a, 10))
+        self.assertFalse(GallopingSearch(a, 10))
 
         a = [404]
 
         for N in [303, 404, 505]:
-            self.assertEqual(bs.GallopingSearch(a, N), N in a)
+            self.assertEqual(GallopingSearch(a, N), N in a)
 
         a = [-404, 505]
 
         for N in [-404, 505, -606, 606]:
-            self.assertEqual(bs.GallopingSearch(a, N), N in a)
+            self.assertEqual(GallopingSearch(a, N), N in a)
 
         a = [-404, 505, 606]
 
         for N in [505, -404, 606, 707]:
-            self.assertEqual(bs.GallopingSearch(a, N), N in a)
+            self.assertEqual(GallopingSearch(a, N), N in a)
 
     def test_abundant(self):
-        bs = BinarySearch([])
-
         for n in range(0, 1000):
             a = self.generate_sorted_array(-10, 10, n)
 
             for N in range(-1000, 1000):
-                self.assertEqual(bs.GallopingSearch(a, N), N in a)
+                self.assertEqual(GallopingSearch(a, N), N in a)
 
     def test_sparse(self):
-        bs = BinarySearch([])
-
         for n in range(0, 1000):
             a = self.generate_sorted_array(-1000, 1000, n)
             #print(a)
 
             for N in range(-1000, 1000):
-                self.assertEqual(bs.GallopingSearch(a, N), N in a)
+                self.assertEqual(GallopingSearch(a, N), N in a)
 
     def test_custom(self):
-        bs = BinarySearch([])
         a = [1, 3, 5, 7, 9, 11, 13]
 
         for N in a:
-            self.assertTrue(bs.GallopingSearch(a, N))
+            self.assertTrue(GallopingSearch(a, N))
 
         for N in [0, 2, 4, 6, 8, 10, 12, 14]:
-            self.assertFalse(bs.GallopingSearch(a, N))
+            self.assertFalse(GallopingSearch(a, N))
 
     def test_left_and_right_boundaries(self):
-        bs = BinarySearch([])
         a = [10, 20, 30, 40, 50]
-        self.assertTrue(bs.GallopingSearch(a, 10))
-        self.assertTrue(bs.GallopingSearch(a, 50))
+        self.assertTrue(GallopingSearch(a, 10))
+        self.assertTrue(GallopingSearch(a, 50))
 
 
 if __name__ == '__main__':
